4966ce2202bacc0bc3e444ab1151e3b763174924
schwiddi <schwiddi@me.com>
2018-08-09 16:16:28 +0200

    some req things

---
+++ require.js
@@ -0,0 +1,15 @@
+// wenn du sachen importiertst
+// sucht node in drei stufen nach dem modul welches du wollst
+
+
+// in diesem fall geht node davon aus das es ein Core modul gibt das http heisst
+const bla = require('http')
+
+// wenn du eigene sachen importierst muss du zwingen mit ./ signalisieren das du ein eigenes modul willst
+const bla2 = require('./app') // das .js mach node selbst hinzu du kannst es aber auch schreiben
+
+// dritte variante
+// node sucht in node_modules / natürlich nur wenn vorhanden
+const bla3 = require('underscore') // da es hier in diesem folder keinen subfolder
+// modules gibt von npm wir das nicht klappen aber trozdem ist es hier beschrieben
+
