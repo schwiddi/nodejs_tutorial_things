12e41ab4e9fe9a7ed5a1ff0245e39c859f4f2b46
schwiddi <schwiddi@me.com>
2018-08-09 16:27:38 +0200

    bla

--- npm-testground/index.js
+++ npm-testground/index.js
@@ -1,11 +1,15 @@
 
 // importing underscore
 // as it was instelled with npm i underscore (--save is no longer needed / it does it automaticly)
-var _ = require('underscore')
+var _ = require('underscore');
 
 // then us underscore.contains to check if a 3 is in the array before
 // writing the output of the function in de var result
-var result = _.contains([1, 2, 3], 3)
+var result = _.contains([1, 2, 3], 3);
 
 // and then log out the result
-console.log(result)
+console.log(result);
+
+// and here the other way around
+var result = _.contains([1, 2, 3], 9);
+console.log(result);
