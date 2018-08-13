4137a6757d4582bc4c11e20c88d39c01936b04f7
schwiddi <schwiddi@me.com>
2018-08-13 19:51:08 +0200

    cleanup after finished leason

--- async-demo/index-5.js
+++ async-demo/index-5.js
@@ -20,9 +20,6 @@ function getAllGamesFromUserCount(user) {
             console.log(`Got Result for getCountAllGamesFromUser from User: ${user} Games: ${count}`);
             resolve(count);
         }, 3000);
-        if (!resolve) {
-            reject();
-        }
     });
 }
 
@@ -33,40 +30,43 @@ function getAllWonGamesFromUserCount(user) {
             console.log(`Got Result for getAllWonGamesFromUserCount from User: ${user} WonGames: ${count}`);
             resolve(count);
         }, 5000);
-        if (!resolve) {
-            reject();
-        }
     });
 }
 
 function calculatePercentageWonGames(all, won) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // reject(new Error('bla'))
+            // return
             const ratio = ( won / all ) * 100;
             console.log(`Got Result for calculatePercentageWonGames from User: Schwiddi Ratio is: ${ratio}`);
             resolve(ratio);
-        }, 10000);
-        if (!resolve) {
-            reject();
-        }
+        }, 1000);
     });
 }
 
 
+// before switching to consume the promises
+// getAllGamesFromUserCount('schwiddi', (countallgamesfromuser) => {
+//     console.log(`callback from function getAllGamesFromUserCount.. the count is ${countallgamesfromuser}`);
+//     const allgames = countallgamesfromuser;
+//     getAllWonGamesFromUserCount('schwiddi', (countallwongamesfromuser) => {
+//         console.log(`callback from function getAllWonGamesFromUserCount.. the count is ${countallwongamesfromuser}`);
+//         const allwongames = countallwongamesfromuser;
+//         calculatePercentageWonGames(allgames, allwongames, (ratio) => {
+//             console.log(`callback from function calculatePercentageWonGames..`);
+//             console.log(`the percentage of won games for user Schwiddi is: ${ratio}`);
+//         });
+//     });
+// });
 
-getAllGamesFromUserCount('schwiddi', (countallgamesfromuser) => {
-    console.log(`callback from function getAllGamesFromUserCount.. the count is ${countallgamesfromuser}`);
-    const allgames = countallgamesfromuser;
-    
-    getAllWonGamesFromUserCount('schwiddi', (countallwongamesfromuser) => {
-        console.log(`callback from function getAllWonGamesFromUserCount.. the count is ${countallwongamesfromuser}`);
-        const allwongames = countallwongamesfromuser;
+// after switch to promises
+getAllGamesFromUserCount('schwiddi')
+    .then(count => getAllWonGamesFromUserCount('schwiddi'))
+    .then(countwon => calculatePercentageWonGames(72,countwon)) // setting 1. arg manualy / my example is not the best to display this
+    .then(ratio => console.log('end of then chain'))
+    .catch(err => console.log('Arrived in .catch', err));
 
-        calculatePercentageWonGames(allgames, allwongames, (ratio) => {
-            console.log(`callback from function calculatePercentageWonGames..`);
-            console.log(`the percentage of won games for user Schwiddi is: ${ratio}`);
-        });
-    });
-});
+
 
 console.log('Echo: After DB Querry in Code..');
